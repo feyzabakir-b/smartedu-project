f90944e7fef5f0ac86233a993ac54ec90a179428
Feyza <feyzabakir51@gmail.com>
2024-07-20 12:00:23 +0300

    The template is divided into partials. Migration was done.

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(request, 'index.html')
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
 
