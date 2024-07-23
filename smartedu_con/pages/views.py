46044384230d0dac2da05a2f86cc28d4ee298d73
Feyza <feyzabakir51@gmail.com>
2024-07-23 09:17:16 +0300

    Contact page created

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 from courses.models import Course
+from . forms import ContactForm
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 # def index(request):
 #     return render(request, 'index.html')
@@ -19,3 +23,13 @@ class IndexView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+class ContactView(SuccessMessageMixin, FormView):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('contact')
+    success_message = 'We received your request'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
